3d0d116eee10f7fea61d2894a6a8735ff80cef35
davidNicolas-cecs <david.nicolasgarcia@student.csulb.edu>
2025-04-19 03:46:58 -0700

    navbar + main display setup

---
+++ client/src/app/(wordle)/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function page() {
+  return (
+    <>
+      {/* Main Container */}
+      <main className="flex flex-col mt-4 items-center justify-items-center  bg-amber-50">
+        {/* Box container  */}
+        <div className="bg-black ">
+          {/* Letter container  */}
+          <div></div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default page;
