a94a0192c9d5131c41f5ed1f145b1c561a975c7e
davidNicolas-cecs <david.nicolasgarcia@student.csulb.edu>
2025-04-20 00:57:03 -0700

    yellow tile added

--- client/src/components/Game/KeyTile.tsx
+++ client/src/components/Game/KeyTile.tsx
@@ -7,18 +7,19 @@ interface KeyTileProps {
   status?: "correct" | "present" | "absent" | "";
 }
 function KeyTile({ char, handleKeyPress, status }: KeyTileProps) {
+  console.log("KeyTile", char, status);
   const bgColor =
     status === "correct"
-      ? "bg-green-500"
+      ? "bg-green-200"
       : status === "present"
       ? "bg-yellow-500"
       : status === "absent"
       ? "bg-gray-500 dark:bg-gray-800"
-      : "";
+      : "dark:bg-gray-700";
   return (
     <button
       disabled
-      className={`bg-gray-400 dark:bg-gray-700 w-[40px] h-[60px] min-w-fit rounded-xs mt-2 p-2 ${bgColor}`}
+      className={`bg-gray-400  w-[40px] h-[60px] min-w-fit rounded-xs mt-2 p-2 ${bgColor}`}
       onClick={() => handleKeyPress(char)}
     >
       <p className="text-black dark:text-white font-black">
